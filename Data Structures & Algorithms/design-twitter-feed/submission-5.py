30008a853ae7fe84ec9ab8ec5a64fab7af6b4c94
Adnan Alhabian <93690965+nannyhab@users.noreply.github.com>
2026-08-24 22:55:54 -0500

    Add: design-twitter-feed - submission-5

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-5.py
@@ -0,0 +1,42 @@
+class Twitter:
+
+    def __init__(self):
+        self.count = 0
+        self.followerMap = defaultdict(set)
+        self.tweetMap = defaultdict(list)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetMap[userId].append([self.count,tweetId])
+        self.count += 1
+        
+    def getNewsFeed(self, userId: int) -> List[int]:
+        maxHeap = []
+        res = []
+
+        self.followerMap[userId].add(userId)
+        for followeeId in self.followerMap[userId]:
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId]) - 1
+                count, tweetId = self.tweetMap[followeeId][index]
+                maxHeap.append([count, tweetId, followeeId, index - 1])
+
+        heapq.heapify_max(maxHeap)
+
+        while maxHeap and len(res) < 10:
+            
+            count, tweetId, followeeId, index = heapq.heappop_max(maxHeap)
+            res.append(tweetId)
+            if index >= 0:
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush_max(maxHeap, [count, tweetId, followeeId, index-1])
+
+        return res
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followerMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followerMap[followerId]:
+            self.followerMap[followerId].remove(followeeId)
+        
